f11897245d23bb1cf1b7cba1e9eebb3aceaa2f18
Khushal Singh Rajawat <singh.prince51@gmail.com>
2017-11-03 09:37:39 +0530

    Moving Route Code to /imports/routes.js

--- client/main.js
+++ client/main.js
@@ -1,50 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import ReactDOM from 'react-dom'
 import { Tracker } from 'meteor/tracker';
+import {routes,onAuthChange} from '../imports/routes/routes';
 
-import Signup from '../imports/ui/Signup';
-import Lnk from '../imports/ui/Link';
-import NotFound from '../imports/ui/NotFound';
-import Login from '../imports/ui/Login';
-
-const unauthenticatedPages = ['/','/signup'];
-const authenticatedPages = ['/links'];
-const onEnterPublicPage = () => {
-  if (Meteor.userId()){
-    browserHistory.replace('/links');
-  }
-};
-
-const onEnterPrivatePage = () => {
-  if (!Meteor.userId()) {
-    browserHistory.replace('/');
-  }
-};
-
-const routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
-    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path="/links" component={Lnk} onEnter={onEnterPrivatePage} />
-    <Route path="*" component={NotFound}/>
-  </Router>
-);
-
-// window.browserHistory = browserHistory;
-
-//Code to Route Pages when logged in and logged out
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
-  const pathname = browserHistory.getCurrentLocation().pathname;
-  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
-  const isAuthenticatedPage = authenticatedPages.includes(pathname);
-  if (isUnauthenticatedPage && isAuthenticated){
-    browserHistory.replace('/links');
-  }else if (isAuthenticatedPage && !isAuthenticated){
-    browserHistory.replace('/');
-  }
 });
 
 Meteor.startup(() => {
